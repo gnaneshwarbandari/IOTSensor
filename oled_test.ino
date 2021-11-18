e9231f491c878b3bbc45512b426948bc2f4ed87e
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2021-11-18 12:10:43 +0530

    Add files via upload

---
+++ oled_test.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#define SSD1306_LCDHEIGHT 64
+#define OLED_ADDR   0x3C
+Adafruit_SSD1306 display(-1);
+//-------- Customise the above values --------
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(40,25);
+  display.println("MEDICINE");
+  display.setCursor(40,35);
+  display.println("REMINDER");
+  display.display();
+}
+void loop() {
+}
